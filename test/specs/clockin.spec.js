ca9ed58b8f0741fb4a360891681f9f00baa35b54
Wilder Gonzales <wilder@mailtrack.io>
2020-03-18 15:33:47 +0100

    move some instructions to functions

--- test/specs/clockin.spec.js
+++ test/specs/clockin.spec.js
@@ -2,24 +2,14 @@ import { credentials, date, day, clockin } from '../../config/config.json';
 
 describe('factorial clock in', () => {
     it('should fill clock in form', () => {
-        //login
+
         browser.url('https://app.factorialhr.com/');
-        $('#user_email').setValue(credentials.email);
-        $('#user_password').setValue(credentials.password);
-        $('.js-submit').click();
+        login(credentials.email, credentials.password);
 
-        browser.waitUntil( () => {
-            return $('.logo___1ofKH').isDisplayed();
-        });
+        openClockInPage(date.year, date.month);
 
-
-        browser.url(`https://app.factorialhr.com/attendance/clock-in/${date.year}/${date.month}`);
-        browser.waitUntil( () => { 
-            return $('.tbody___2dGKu').isDisplayed();
-        });
-
-        let i = 0;
-        let n = 0;
+        let i = 0; // shift index
+        let n = 0; // row index
         let max = $$('.normal___354i1.small___2lrXH.noLabel___wEbd9').length * 2;
         let plusBtn = $$('.default___2GP40.terciary___20b_P');
         let day = $$('.weekDay___26AC_');
@@ -35,31 +25,39 @@ describe('factorial clock in', () => {
                 break;
             }
 
-            if (day[n].getText() !== 'Sunday' && day[n].getText() !== 'Saturday' && leaves[n].getText() !== 'Holidays') {
+            if (!isWeekend(day[n]) && !isHoliday(leaves[n])) {
+                // set worked shifts
+                if(isFriday(day[n]) && clockin.intensive) {
+                    setWorkedShifts(fields, i, clockin.morningStart, clockin.fridayEnd);
+                } else {
 
-                fields[i].setValue(clockin.morningStart);
-                fields[i+1].addValue(clockin.morningEnd);
+                    setWorkedShifts(fields, i, clockin.morningStart, clockin.morningEnd);
 
-                //click submit
-                $('.submit___IEW1l').click();
+                    //click submit
+                    $('.submit___IEW1l').click();
 
-                if(closeSuccessTooltip() === false){
-                    break;
-                }
+                    if(closeSuccessTooltip() === false){
+                        break;
+                    }
 
-                //click (+)
-                plusBtn[n].click();
+                    //click (+): add shifts
+                    plusBtn[n].click();
 
-                fields = $$('.normal___354i1.small___2lrXH.noLabel___wEbd9');
+                    fields = $$('.normal___354i1.small___2lrXH.noLabel___wEbd9');
 
-                fields[i+2].setValue(clockin.afternoonStart);
-                fields[i+3].addValue(clockin.afternoonEnd);
+                    // set worked shifts
+                    if(isFriday(day[n])) {
+                        setWorkedShifts(fields, i+2, clockin.afternoonStart, clockin.fridayEnd);
+                    } else {
+                        setWorkedShifts(fields, i+2, clockin.afternoonStart, clockin.afternoonEnd);
+                    }
 
-                //click submit
-                $('.submit___IEW1l').click();
+                    //click submit
+                    $('.submit___IEW1l').click();
 
-                if(closeSuccessTooltip() === false){
-                    break;
+                    if(closeSuccessTooltip() === false){
+                        break;
+                    }
                 }
 
                 i += 4;
@@ -74,6 +72,28 @@ describe('factorial clock in', () => {
     })
 })
 
+const login = (email, password) => {
+    $('#user_email').setValue(email);
+    $('#user_password').setValue(password);
+    $('.js-submit').click();
+
+    browser.waitUntil( () => {
+        return $('.logo___1ofKH').isDisplayed();
+    });
+}
+
+const openClockInPage = (year, month) => {
+    browser.url(`https://app.factorialhr.com/attendance/clock-in/${year}/${month}`);
+    browser.waitUntil( () => {
+        return $('.tbody___2dGKu').isDisplayed();
+    });
+}
+
+const setWorkedShifts = (elements, index, start, end) => {
+    elements[index].setValue(start);
+    elements[index+1].addValue(end);
+}
+
 const closeSuccessTooltip = () => {
     try {
         browser.waitUntil( () => {
@@ -83,4 +103,28 @@ const closeSuccessTooltip = () => {
     } catch {
         return false;
     }
+}
+
+const isWeekend = (element) => {
+    if(element.getText() === 'Saturday' || element.getText() === 'Sunday') {
+        return true;
+    }
+
+    return false;
+}
+
+const isHoliday = (element) => {
+    if(element.getText() === 'Holidays') {
+        return true;
+    }
+
+    return false;
+}
+
+const isFriday = (element) => {
+    if(element.getText() === 'Friday') {
+        return true;
+    }
+
+    return false;
 }
